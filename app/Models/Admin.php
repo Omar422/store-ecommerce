59ce8aecde7a21b1e80765c91b627af48e221b6c
Omar422 <amoor350om@hotmail.com>
2022-10-24 14:23:53 +0300

    end begin branch

---
+++ app/Models/Admin.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+// class Admin extends Model
+class Admin extends Authenticatable
+{
+    //
+    // protected $table = "admins"; // it's with laravel con
+    protected $guarded = []; // all fields
+    public $timestamps = true;
+}
