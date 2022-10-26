8eae9e321a2b78cea586e7d7b2c339aa3dc420af
Omar422 <amoor350om@hotmail.com>
2022-10-26 07:57:06 +0300

    =end shippings

--- app/Http/Controllers/Dashboard/SettingsController.php
+++ app/Http/Controllers/Dashboard/SettingsController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Setting;
 use App\Http\Requests\ShippingsRequest;
 use Illuminate\Http\Request;
+use DB;
 
 class SettingsController extends Controller
 {
@@ -33,6 +34,29 @@ class SettingsController extends Controller
     }
 
     public function updateShippingMethods(ShippingsRequest $request, $id) {
-        return $request;
+        // return $request;
+
+        // validation in ShippingRequest request
+
+        try {
+            // update
+            $shipping_method = Setting::find($id);
+
+            // transaction => apply all or nothing
+            DB::transaction();
+
+            $shipping_method -> update([
+                'plain_value'   => $request -> plain_value,
+            ]);
+            $shipping_method -> value = $request -> value;
+            $shipping_method -> save();
+
+            DB::commit();
+
+            return redirect() -> back() -> with(['success' => 'تم التحديث بنجاح']);
+        } catch (\Exception $ex) {
+            return redirect() -> back() -> with(['error' => 'هناك خطأ ما، حاول مرة أخرى']);
+            DB::rollback();
+        }
     }
 }
